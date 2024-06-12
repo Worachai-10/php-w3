2c1d1df43c63d512202b8ba2705d93e1c5a7fa94
Worachai-10 <139629440+Worachai-10@users.noreply.github.com>
2024-06-12 14:55:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>610-10 วรชัย</title>
+</head>
+<body>
+    <?php
+        $a = "3";
+        $b = "4";
+        $A = "20.5";
+        $B = "20.5";
+        $C = $A == $B;
+        echo "<b>";
+        echo "ผลลัพธ์";
+        echo "</br>";
+        echo "</b>";
+        echo "ค่าของ \$a = 3"."</br>"."ค่าของ \$b = 4";
+        echo "</br>";
+        echo "ผลรวมของ \$a และ \$b "." คือ ".$a + $b;
+        echo "</br>";
+        echo "</br>";
+        echo "<b>";
+        echo "ผลลัพธ์"."</br>";
+        echo "</b>";
+        echo "\$A = B"."</br>";
+        echo "\$A = 20.5"."</br>";
+        echo "\$B = 20.5"."</br>";
+        echo "คำตอบคือ ".$C;
+    ?>
+</body>
+</html>
